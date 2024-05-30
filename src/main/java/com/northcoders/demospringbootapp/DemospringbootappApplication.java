a52012a272880de6fe49d3f7cbbfd823e8fdd192
ste <mrbarrow@hotmail.co.uk>
2024-05-30 10:49:37 +0100

    initial commit

---
+++ src/main/java/com/northcoders/demospringbootapp/DemospringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.demospringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringbootappApplication.class, args);
+	}
+
+}
