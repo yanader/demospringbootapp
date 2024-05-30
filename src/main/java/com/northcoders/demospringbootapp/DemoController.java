ae28e676d532f1558e02f71aa498d45cc1214a28
Jimmyrai321 <jimmyrai321@gmail.com>
2024-05-30 11:23:09 +0100

    feat: done task 12

--- src/main/java/com/northcoders/demospringbootapp/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/DemoController.java
@@ -1,8 +1,14 @@
 package com.northcoders.demospringbootapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.northcoders.demospringbootapp.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("api/v1")
@@ -12,6 +18,22 @@ public class DemoController {
     public String getGreeting(){
         return "Hello there!";
     }
+
+
+    @GetMapping("/people")
+    public List<Person> getPeople(){
+        return initPeople();
+    }
+
+    private List<Person> initPeople(){
+        List<Person> pplList = new ArrayList<>();
+        pplList.add(new Person("Bob",28,"bob321@gmail.com","London","pizza"));
+        pplList.add(new Person("Angela",30,"angela.valdez@outlook.com","Manchester","ramen"));
+        return pplList;
+    }
+
+
+
 
 
 
---
+++ src/main/java/com/northcoders/demospringbootapp/model/Person.java
@@ -0,0 +1,4 @@
+package com.northcoders.demospringbootapp.model;
+
+public record Person(String name,int age,String emailAddress,String placeOfBirth,String favFood) {
+}
