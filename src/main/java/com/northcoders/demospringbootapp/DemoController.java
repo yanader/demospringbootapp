7eddbb9097bc48870101abc9f381d1ccaed1eb6b
Jimmyrai321 <jimmyrai321@gmail.com>
2024-05-30 11:05:50 +0100

    feat: done task 10

--- src/main/java/com/northcoders/demospringbootapp/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/DemoController.java
@@ -1,7 +1,19 @@
 package com.northcoders.demospringbootapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class DemoController {
+
+    @GetMapping("/hello")
+    public String getGreeting(){
+        return "Hello there!";
+    }
+
+
+
+
 }
